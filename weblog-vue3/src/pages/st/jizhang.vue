718c7177c38adcd785de69e28265e39cdb843f33
35811 <aaa@qq.com>
2024-05-23 14:32:57 +0800

    test

--- weblog-vue3/src/pages/st/jizhang.vue
+++ weblog-vue3/src/pages/st/jizhang.vue
@@ -11,6 +11,35 @@
     <el-button type="primary" class="ml-3" :icon="Search" @click="getTableData">查询</el-button>
     <el-button class="ml-3" :icon="RefreshRight" @click="reset">重置</el-button>
   </el-card>
+  <el-table :data="tableData" stripe style="width: 100%" class="mt-4" v-loading="tableLoading">
+            <el-table-column prop="title" label="标题" width="380" />
+            <el-table-column label="预览图" width="180">
+                <template #default="scope">
+                    <el-image style="width: 50px;" :src="scope.row.titleImage" />
+                </template>
+            </el-table-column>
+            <el-table-column prop="createTime" label="发布时间" width="180" />
+            <el-table-column label="操作">
+                <template #default="scope">
+                    <el-button size="small" @click="showArticleUpdateEditorShow(scope.row)">
+                        <el-icon class="mr-1">
+                            <Edit />
+                        </el-icon>
+                        编辑</el-button>
+                    <el-button size="small" @click="previewArticle(scope.row)">
+                        <el-icon class="mr-1">
+                            <View />
+                        </el-icon>
+                        预览</el-button>
+                    <el-button type="danger" size="small" @click="deleteArticleSubmit(scope.row)">
+                        <el-icon class="mr-1">
+                            <Delete />
+                        </el-icon>
+                        删除
+                    </el-button>
+                </template>
+            </el-table-column>
+        </el-table>
 </template>
 
 <script setup>
